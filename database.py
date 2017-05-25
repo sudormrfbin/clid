ed27a4b0ef5e0d5a73192c23efe78ebb30242684
Gokul <gokulps15@gmail.com>
2017-05-25 13:48:25 +0530

    version 0.1 - view and search for files

---
+++ database.py
@@ -0,0 +1,44 @@
+"""Contains database objects for helping clid"""
+
+import os
+import glob
+
+import npyscreen
+
+
+BASE_DIR = '/mnt/0b6b53eb-f83a-4f34-9224-da2310643534/Music/'
+
+
+class Mp3DataBase(npyscreen.NPSFilteredDataBase):
+    """Class to manage the structure of mp3 files in BASE_DIR.
+
+       Attributes:
+            file_dict(dict):
+                dict with the filename as key and absolute path to
+                file as value. Used for displaying files and changing
+                metadata
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.get_list()   # set file_dict attribute
+        self._values = tuple(sorted(self.file_dict.keys()))   # sorted tuple of filenames
+
+ # IDEA: set_values and set_search_list for updating values and search_list when refreshed
+
+    def get_list(self):
+        """Get a list of mp3 files in BASE_DIR recursively, make a dict
+           out of it and assign it to file_dict
+        """
+        ret_list = []
+        for dir_tree in os.walk(BASE_DIR):   # get all mp3 files in the dir and sub-dirs
+            ret_list.extend(glob.glob(dir_tree[0] + '/' + '*.mp3'))
+
+        # make a dict with the basename as key and absolute path as value
+        self.file_dict = dict([(os.path.basename(abspath), abspath) for abspath in ret_list])
+
+    def filter_data(self):
+        if self._filter and self._values:
+            return [mp3 for mp3 in self.get_all_values() if self._filter in mp3.lower()]
+        else:
+            return self.get_all_values()
--- main.py
+++ main.py
@@ -1,26 +1,87 @@
-from glob import glob
-import npyscreen as np
+"""Clid is an app to edit the id3v2 tags of mp3 files from the
+   command line.
+"""
 
-mp3_list = glob('/mnt/0b6b53eb-f83a-4f34-9224-da2310643534/Music/*.mp3')
+__version__ = '0.1'
 
-class CommandLine(np.ActionControllerSimple):
+import curses
+
+import database
+import npyscreen as npy
+
+
+class CommandLine(npy.ActionControllerSimple):
+    """Command line at the bottom."""
+
     def create(self):
-        self.add_action('^:q', self.exit_app, live=False)
+        self.add_action('^:q', self.exit_app, live=False)   # quit with ':q'
+        self.add_action('^/.+', self.search_for_files, live=True)
 
     def exit_app(self, command_line, widget_proxy, live):
+        """Exit from the app."""
+        # args are used internally by npy
         exit()
 
+    def search_for_files(self, command_line, widget_proxy, live):
+        """Search for files while given a string"""
+        if len(command_line[1:]) > 2:
+            self.parent.value.set_filter(command_line[1:])
+        else:
+            self.parent.value.set_filter(None)
 
-class ClidInterface(np.FormMuttActiveTraditional):
+        self.parent.after_search_now_filter_view = True
+        self.parent.wMain.values = self.parent.value.get()
+        self.parent.wMain.display()
+
+
+class ClidMultiline(npy.MultiLine):
+    """MultiLine class to be used by clid. `Esc` has been modified to revert
+       the screen back to the normal view after a searh has been performed
+       (the search results will be shown; blank if no matches are found)
+
+       Note:
+            self.parent refers to ClidInterface -> class
+    """
+    def set_up_handlers(self):
+        super().set_up_handlers()
+        self.handlers[curses.ascii.ESC] = self.h_revert_escape
+
+    def h_revert_escape(self, char):
+        """Handler which switches from the filtered view of search results
+           to the normal view with the complete list of files.
+        """
+        if self.parent.after_search_now_filter_view:   # if screen is showing search results
+            self.parent.wMain.values = self.parent.value.get_all_values()   # display all the values
+            self.parent.after_search_now_filter_view = False
+# TODO: make it faster
+# IDEA: use search_for_files to reset(/a to command_line ?)
+
+class ClidInterface(npy.FormMuttActiveTraditional):
+    """The main app with the ui.
+
+       Note:
+            self.value refers to an instance of DATA_CONTROLER
+    """
+    ACTION_CONTROLLER = CommandLine
+    MAIN_WIDGET_CLASS = ClidMultiline
+    DATA_CONTROLER = database.Mp3DataBase
+
     def create(self):
-        self.action_controller = CommandLine
-        self.wStatus1.value = 'clid'
-        self.wStatus2.value = 'Status Bar'
+        super().create()   # so that status widgets are created
+        self.set_value(self.DATA_CONTROLER())   # isn't automatically done(open issue ?)
+
+        # used to revert screen(ESC) to standard view after a search has been done
+        # see class ClidMultiline
+        self.after_search_now_filter_view = False
+
+        self.wStatus1.value = 'clid v0.1 '
+        self.wStatus2.value = 'clid status line '
+        self.wMain.values = self.value.get()
 
 
-class ClidApp(np.NPSAppManaged):
+class ClidApp(npy.NPSAppManaged):
     def onStart(self):
-        self.addForm('MAIN', ClidInterface, name='clid')
+        self.addForm("MAIN", ClidInterface)
 
 
 if __name__ == '__main__':
