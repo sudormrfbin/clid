34a0f8b11b3d35bcb09e12274a2310bd1621561d
Gokul <gokulps15@gmail.com>
2018-02-08 10:40:55 +0530

    refactor

--- clid/database/musicdb.py
+++ clid/database/musicdb.py
@@ -5,14 +5,15 @@
 
 import os
 import fnmatch
+import itertools
 
 class MusicDataBase:
     """Manages music files
        Attributes:
             music_dir: Path to base directory with audio files
-            music_files(dict):
+            _music_files(dict):
                 Mapping with filetype as key and list of files as value
-                music_files -> {'mp3': ['a/b/c.mp3'], 'ogg': ['e/f/g.ogg']}
+                _music_files -> {'mp3': ['a/b/c.mp3'], 'ogg': ['e/f/g.ogg']}
     """
     def __init__(self, music_dir=None):
         self.set_music_dir(music_dir=music_dir)
@@ -23,16 +24,19 @@ class MusicDataBase:
             self.music_dir = os.path.expanduser(music_dir)
         except AttributeError:   # music_dir -> None
             self.music_dir = None
+            self._music_files = None
             return None
 
         def find_files(ext):
-            """Find audio files with extension `ext` in music_dir. Returns a list."""
+            """Find audio files with extension `ext` in music_dir. Returns a list
+               sorted lexicographically
+            """
             pattern = '*.{ext}'.format(ext=ext)   # make glob pattern to use with fnmatch
             files_found = []
             for dirpath, __, files in os.walk(self.music_dir, followlinks=True):
                 files_here = fnmatch.filter(names=files, pat=pattern)
                 files_found.extend([os.path.join(dirpath, file) for file in files_here])
-            return files_found
+            return sorted(files_found, key=self.get_basename)
 
         # NOTE: Paths are not canonical
         self._music_files = {
@@ -45,5 +49,5 @@ class MusicDataBase:
            every file
         """
         if ext == 'all':
-            return sum(self._music_files.values(), [])   # merge all lists
+            return list(itertools.chain(*self._music_files.values()))   # merge all lists
         return self._music_files[ext].copy()
--- tests/test_databases.py
+++ tests/test_databases.py
@@ -2,28 +2,54 @@
 
 import os
 
-import clid.database.musicdb
+import pytest
+
+import clid.database
 
 
 HERE = os.path.dirname(__file__)
 
+@pytest.fixture
+def mp3_files():
+    with open(os.path.join(HERE, 'mp3_files.txt')) as f:
+        return sorted(f.read().splitlines())
+
+
 class TestMusicDB:
-    db = clid.database.musicdb.MusicDataBase()
 
-    def get_mp3_files_list_from_txt_file(self):
-        """Return names of mp3 files stored in mp3_files.txt in a list"""
-        with open(os.path.join(HERE, 'mp3_files.txt')) as f:
-            return sorted([name.replace('\n', '') for name in f.readlines()])
+    @pytest.fixture(scope='session')
+    def musicdb(self):
+        return clid.database.MusicDataBase()
 
-    def test_set_music_dir(self):
-        self.db.set_music_dir(os.path.join(HERE, 'samples'))
-        mp3_files = self.get_mp3_files_list_from_txt_file()
-        assert mp3_files == sorted(self.db._music_files['mp3'])
+    @pytest.fixture
+    def f_musicdb(self):
+        db = clid.database.MusicDataBase()
+        db._music_files = {
+            'mp3': [
+                'a/b/F.R.I.E.N.D.S.mp3',
+                'a/b/d/FRIENDS.mp3',
+                'a/b/c/friends.mp3',
+            ],
+            'ogg': [
+                'a/b/fRiEnDs.ogg',
+                'a/b/c/friends.ogg',
+            ]
+        }
+        return db
+
+    def test_set_music_dir(self, musicdb, mp3_files):
+        musicdb.set_music_dir(os.path.join(HERE, 'samples'))
+        assert mp3_files == sorted(musicdb._music_files['mp3'])
 
     # TODO: add ogg files
-    def test_get_files(self):
-        assert sorted(self.db.get_files(ext='all')) == \
-                self.get_mp3_files_list_from_txt_file()
+    def test_get_files(self, musicdb, mp3_files):
+        assert mp3_files == sorted(musicdb.get_files(ext='all'))
+
+    def test_file_path_are_absolute(self, musicdb):
+        # assert [f for f in self.db.get_files('mp3') if os.path.isabs(f)]
+        assert all([True if os.path.isabs(f) else False for f in musicdb.get_files('mp3')])
+
+    def test_simple_filename_search_empty_str(self, f_musicdb):
 
     def test_file_path_are_absolute(self):
         assert [f for f in self.db.get_files('mp3') if os.path.isabs(f)]
