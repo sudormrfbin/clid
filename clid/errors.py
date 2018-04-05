585b2a2ce8226481b44c331236637b9599320813
Gokul <gokulps15@gmail.com>
2018-04-05 10:25:44 +0530

    added basic custom exceptions

---
+++ clid/errors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Exceptions used by Clid"""
+
+class ClidError(Exception):
+    """Base class for all Clid exceptions."""
+    pass
+
+
+class ClidUserError(ClidError):
+    """Exception raised due to user's behavior."""
+    pass
