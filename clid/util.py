f8118ad28b7992ba5123a1ee9e2662cb6063d0b7
Gokul <gokulps15@gmail.com>
2018-09-30 00:08:30 +0530

    add user's data directory and config file path

--- clid/util.py
+++ clid/util.py
@@ -8,6 +8,11 @@ Utilities for clid.
 
 from os import path
 
+import confuse
+
 _HERE = path.dirname(__file__)
-DATA_DIR = path.join(_HERE, "data")
-DEFAULT_CONFIG = path.join(DATA_DIR, "config.yaml")
+CLID_DATA_DIR = path.join(_HERE, "data")
+DEFAULT_CONFIG = path.join(CLID_DATA_DIR, "config.yaml")
+
+USER_DATA_DIR = path.join(confuse.config_dirs()[0], "clid")
+USER_CONFIG = path.join(USER_DATA_DIR, "config.yaml")
