19850b4b32df3ff5a67b633aa200a1cb54f72263
Gokul <gokulps15@gmail.com>
2018-11-03 21:14:26 +0530

    normalize docstrings in musicdb.py

--- clid/musicdb.py
+++ clid/musicdb.py
@@ -25,19 +25,23 @@ else:
 
 
 class MusicDataBase:
-    """Manages music files
-       Attributes:
-            music_dir: Path to base directory with audio files
-            _music_files(dict):
-                Mapping with filetype as key and list of files as value
-                _music_files -> {'mp3': ['a/b/c.mp3'], 'ogg': ['e/f/g.ogg']}
+    """
+    Manages music files.
+
+    Attributes:
+        music_dir(str or None): Path to base directory with audio files.
+        _music_files(dict):
+            Mapping with filetype as key and list of files as value, like
+            {'mp3': ['a/b/c.mp3'], 'ogg': ['e/f/g.ogg']}
     """
 
     def __init__(self, music_dir=None):
         self.set_music_dir(music_dir=music_dir)
 
     def set_music_dir(self, music_dir):
-        """Set `music_dir` attr and scan the dir for audio files"""
+        """
+        Set `music_dir` attribute and scan the directory for audio files
+        """
         try:
             music_dir = os.path.expanduser(music_dir)
             if not os.path.isdir(music_dir):
@@ -51,8 +55,9 @@ class MusicDataBase:
             return None
 
         def find_files(ext):
-            """Find audio files with extension `ext` in music_dir. Returns a list
-               sorted lexicographically
+            """
+            Find audio files with extension `ext` in music_dir. Returns a list
+            sorted lexicographically.
             """
             ext = "." + ext
             files_found = []
@@ -70,8 +75,16 @@ class MusicDataBase:
         self._music_files = {"mp3": find_files("mp3"), "ogg": find_files("ogg")}
 
     def get_files(self, ext="all"):
-        """Return filenames with extension `ext`. If ext is 'all', return
-           every file
+        """
+        Get files from the database.
+
+        Args:
+            ext (str):
+                The type of files to retrieve filtered by extension. If value
+                is "all", return every file.
+
+        Returns:
+            list: List of absolute filenames.
         """
         if ext == "all":
             return list(itertools.chain(*self._music_files.values()))  # merge all lists
@@ -79,9 +92,19 @@ class MusicDataBase:
 
     @staticmethod
     def splitext(path):
-        """Return the namedtuple (filename, ext). This function is like
-           os.path.splitext, except that it will return the basename and
-           extension(without the dot) in a named tuple.
+        """
+        Similar to os.path.splitext, but instead returns a `namedtuple`.
+
+        Args:
+            path (str): Path to be splitted.
+
+        Returns:
+            collections.namedtuple:
+                A `Path` namedtuple, with a `filename` and `ext` attribute.
+
+        Note:
+            the `ext` attribute is stripped of the dot which seperates the
+            filename and extension.
         """
         namedtuple = collections.namedtuple("Path", ["filename", "ext"])
 
@@ -90,14 +113,22 @@ class MusicDataBase:
         return namedtuple(filename=name, ext=ext)
 
     def get_basename(self, path):
-        """Return only the filename, without extension"""
+        """
+        Get the filename only, without extension.
+        """
         return self.splitext(path).filename
 
     def filename_search(self, text, ignore_case, fuzzy_search, ext="all"):
-        """Search filenames which match the search text
-           ignore_case -> bool: ignored if fuzzy_search is True
-           fuzzy_search -> bool: whether to use fuzzy searching
-           ext -> str: extensions to search for; 'mp3', 'ogg', etc
+        """
+        Search filenames for a match with the search text.
+
+        Args:
+            text (str): Text to search for.
+            ignore_case (bool):
+                Whether to consider case. This parameter is ignored if
+                the fuzzy_search parameter is True.
+            fuzzy_search (bool): Whether to use fuzzy searching.
+            ext (str): Extensions to search for; 'mp3', 'ogg', etc.
         """
         files_to_search = self.get_files(ext=ext)
 
@@ -118,8 +149,20 @@ class MusicDataBase:
             return [file for file in files_to_search if text in self.get_basename(file)]
 
     def sort(self, files, sortby=None, reverse=False):
-        """Sort `files` by the condition `sortby`. Reverse the sort if `reverse`
-           sortby -> 'ext', 'name', 'mod_time'
+        """
+        Sort `files` by a condition.
+
+        Args:
+            files (list of str): Paths to be sorted.
+            sortby (str):
+                Condition to sort the paths. Possible value:
+                "ext": By extension.
+                "name": By the filename (by basename, not full path).
+                "mod_time": By last modified time.
+            reverse (bool): Whether to reverse the sorted list.
+
+        Returns:
+            list: The sorted list of paths.
         """
         if sortby is None:
             return files
